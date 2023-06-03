d392bd5f16d98a2f16affefde0d1352db6ec19be
ricunidad <ricunidad@gmail.com>
2023-06-03 01:07:10 -0400

    Se agrego styled components, y se diseñó la página inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato',sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto auto;
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt="imagenes criptomonedas"
+      />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+
+        <Formulario />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </Contenedor>
   )
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import styled from '@emotion/styled'
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #7A7DFE;
+    }
+`
+
+const Formulario = () => {
+  return (
+    <form action="">
+        <InputSubmit
+            type='submit'
+            value='Cotizar'
+        />
+    </form>
+  )
+}
+
+export default Formulario
