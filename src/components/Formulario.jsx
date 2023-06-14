56e775cf5c6b6c2f9d3c910de6f8d5b84b5ad267
ricunidad <ricunidad@gmail.com>
2023-06-13 23:46:58 -0400

    Se creó el hook para los campos de tipo select, y la data del filtro de moneda

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,8 @@
 
 import React from 'react'
 import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -13,6 +15,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover {
         background-color: #7A7DFE;
@@ -20,8 +23,17 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+
+
+  const [SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas);
+  //const [SelectCriptomonedas] = useSelectMonedas('Elige tu Criptomoneda');
+
+
+
   return (
     <form action="">
+        <SelectMonedas />
+
         <InputSubmit
             type='submit'
             value='Cotizar'
