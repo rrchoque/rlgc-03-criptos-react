d38384564d29ede50380854bd4ffaf7b378aa686
ricunidad <ricunidad@gmail.com>
2023-06-15 01:34:10 -0400

    Se agregó Spinner de carga

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -45,17 +46,22 @@ function App() {
 
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
         const { moneda, criptomoneda } = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
 
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
-
+        
         setResultado(resultado.DISPLAY[criptomoneda][moneda])
+
+        setCargando(false)
       }
 
       cotizarCripto()
@@ -76,6 +82,7 @@ function App() {
           setMonedas={setMonedas}
         />
 
+        { cargando && <Spinner /> }
         { resultado.PRICE && <Resultado resultado={resultado} /> }
       </div>
       
